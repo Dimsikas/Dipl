794e7b14c21bc05d4e047f1d27b16019aeea9307
Professional <Professional@USER-PC>
2023-03-26 23:43:26 +0300

    Добавьте файлы проекта.

---
+++ курсач/Д1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace курсач
+{
+    /// <summary>
+    /// Логика взаимодействия для Д1.xaml
+    /// </summary>
+    public partial class Д1 : Window
+    {
+        public Д1()
+        {
+            InitializeComponent();
+        }
+    }
+}
